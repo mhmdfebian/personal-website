aa6e42e5c26b90daf17f7b0a0d36a52e7201db03
bian <bian@192.168.1.5>
2026-09-12 07:47:27 +0700

    change-bg

--- components/layout/site-header.tsx
+++ components/layout/site-header.tsx
@@ -6,11 +6,21 @@ export function SiteHeader() {
   return (
     <header className="relative border-b border-black/10 bg-white">
       <div className="mx-auto flex w-full max-w-6xl items-center justify-between px-6 py-5">
-        <Link className="font-semibold tracking-tight" href="/">
-          Personal website
+        
+        <Link href="/" className="flex items-center">
+          <Image
+            src="/logo-bn.png"
+            alt="BN Logo"
+            width={50}
+            height={40}
+            priority
+            className="h-10 w-auto object-contain"
+          />
         </Link>
+
         <SiteNav />
         <MobileNav />
+
       </div>
     </header>
   );
