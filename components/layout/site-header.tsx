4fc1bf3f3dca6753d90fa2985d8ae09bc97451e0
bian <bian@192.168.1.5>
2026-09-13 12:58:44 +0700

    navbar

--- components/layout/site-header.tsx
+++ components/layout/site-header.tsx
@@ -5,10 +5,10 @@ import Image from "next/image";
 
 export function SiteHeader() {
   return (
-    <header className="absolute left-0 top-0 z-50 w-full px-6 pt-5">
-      <div className="mx-auto flex w-fit items-center gap-8 rounded-full border border-black/5 bg-white/95 px-5 py-2.5 shadow-[0_2px_15px_rgba(0,0,0,0.08)] backdrop-blur-md">
+    <header className="fixed left-0 top-0 z-50 w-full px-6 pt-5">
+      <div className="mx-auto flex w-fit items-center gap-8 rounded-full border border-black/5 bg-white/75 px-5 py-2.5 shadow-[0_2px_15px_rgba(0,0,0,0.08)] backdrop-blur-md">
+
         <div className="mr-40">
-          {/* Logo */}
           <Link href="/" className="flex shrink-0 items-center">
             <Image
               src="/bn-logo-transparent.png"
@@ -21,10 +21,7 @@ export function SiteHeader() {
           </Link>
         </div>
 
-        {/* Desktop Navigation */}
         <SiteNav />
-
-        {/* Mobile Navigation */}
         <MobileNav />
 
       </div>
